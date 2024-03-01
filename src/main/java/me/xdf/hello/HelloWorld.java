7aab694a1ea44281fc815b80d9e8173088f7eccb
CVMaster <1533421618@qq.com>
2024-03-01 15:14:47 +0800

    测试MySQL连接

---
+++ src/main/java/me/xdf/hello/HelloWorld.java
@@ -0,0 +1,25 @@
+package me.xdf.hello;
+
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+
+public class HelloWorld {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/dbjsp";
+        String user = "root";
+        String password = "1234";
+
+        Connection connection = DriverManager.getConnection(url, user, password);
+        Statement statement = connection.createStatement();
+
+        String sql = "SELECT * FROM student;";
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()){
+            System.out.print("pId=" + resultSet.getString("pId"));
+            System.out.println("\tname=" + resultSet.getString("name"));
+        }
+    }
+}
